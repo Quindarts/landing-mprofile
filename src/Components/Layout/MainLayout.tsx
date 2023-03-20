3ea16214fcbbfa980d96831bad668e8a58ace81d
duongidle <pervertcat2k2@gmail.com>
2023-03-20 22:08:36 +0700

    moi sua loi chinh ta :V

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -2,21 +2,20 @@ import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-import Sidebar from "./Siderbar";
+import Sidebar from "./Sidebar";
 
 function MainLayout() {
-    return (
-        <Fragment>
-            <Sidebar />
-            <Header />
-            <div className="main-layout">
-                
-                MainLayout
-                <Outlet />
-            </div>
-            <Footer />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Sidebar />
+      <Header />
+      <div className="main-layout">
+        MainLayout
+        <Outlet />
+      </div>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default MainLayout;
